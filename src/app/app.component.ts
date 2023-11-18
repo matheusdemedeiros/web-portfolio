903f988c0e56ea18c16623ead5757c0d26fb2d5e
Matheus <matheusdemedeiros200599@gmail.com>
2023-11-18 18:31:10 -0300

    feat: primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Matheus de Medeiros';
+
+  constructor(titleService: Title) {
+
+    titleService.setTitle(this.title);
+  }
 }
